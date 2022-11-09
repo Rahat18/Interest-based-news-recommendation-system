a6cba5370f471f00845d7804d99037cd2d2c0fcf
Rahat <anjumrahat1618@gmail.com>
2022-11-09 16:01:28 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { MainAppComponent } from './main-app/main-app.component';
+
+
+const appRoutes:Routes=[
+  
+  
+  {path : 'login' , component: LoginComponent},
+  {path : 'main-app' , component: MainAppComponent},
+  {path:'signup',component:SignupComponent},
+  {path:'',redirectTo:'login',  pathMatch: 'full' }
+  
+  
+];
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    MainAppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

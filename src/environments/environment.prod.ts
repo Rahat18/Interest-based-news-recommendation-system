28af25d4562eb195f1e852a176bc00ff5971e898
Ankit Gupta <ag3831124@gmail.com>
2023-02-28 14:39:41 +0530

    fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: "https://http-nodejs-production-dfb7.up.railway.app/v1/api"
+  // baseUrl: "https://http-nodejs-production-dfb7.up.railway.app/v1/api",
+  baseUrl: "https://news-recommendation-system.onrender.com/v1/api"
 };
